b473aca2ddc1f4e9a322f4175854f82ea833806e
stylesuxx <stylesuxx@gmail.com>
2013-10-13 16:03:22 +0200

    removed api token, since this now comes from the user table.

--- includes/settings.php
+++ includes/settings.php
@@ -1,9 +1,8 @@
 <?php
 $db_host = 'localhost';
-$db_name = 'beercounter';
+$db_name = 'beveragecounter';
 $db_user = 'root';
 $db_pass = 'xxxxxx';
 
-$api_token = 'HERE_COME_THE_API_TOKEN';
 $api_public = False;
 ?>
