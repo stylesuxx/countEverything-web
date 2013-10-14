f6fe394abb883ee74f5254fe5f4490ecaea38249
stylesuxx <stylesuxx@gmail.com>
2013-10-14 21:47:19 +0200

    Refactored api into a seperate file. Return JSON on api request

---
+++ api.php
@@ -0,0 +1,64 @@
+<?php
+include 'includes/settings.php';
+include 'includes/database.php';
+$db = new Database($db_host, $db_name, $db_user, $db_pass);
+
+function returnData() {
+
+}
+
+/**
+ * Handle API requests and return JSON and set according http status code.
+ * 
+ * 400 when invalid action
+ * 401 when token not valid
+ * 200 when successfull request
+ *
+ * test a connection: ?action=test&token=asdf_1234
+ * add a beverage: ?action=add&token=asdf_1234&beverage=beer&amount=0.5
+ */
+if(isset($_GET['action']) && isset($_GET['token'])){
+  $action = $_GET['action'];
+  $token = $_GET['token'];
+  if($db->isValidToken($token)){
+    $user = $db->getUserId($token);
+    switch($action){
+      case 'add':{
+        if(isset($_GET['beverage']) && isset($_GET['amount']) && is_numeric($_GET['amount'])){
+          $db->addBeverage($_GET['beverage'], $_GET['amount'], $user);
+          header('HTTP/1.1 200 OK');
+          echo json_encode(array(
+          	'http' => 200,
+          	'message' => 'Added beverage',
+          	));
+          exit;
+        }
+        else{
+          header('HTTP/1.1 400 Bad Request');
+          echo json_encode(array(
+          	'http' => 400,
+          	'message' => 'Bad request',
+          	));
+          exit;
+        }
+      } break;
+      case 'test':{
+        header('HTTP/1.1 200 OK');
+	    echo json_encode(array(
+	    	'http' => 200,
+	        'message' => 'Everything set up',
+	        ));
+	    exit;
+      } break;
+    }
+  }
+  else{
+    header('HTTP/1.1 401 Unauthorized');
+    echo json_encode(array(
+    	'http' => 401,
+        'message' => 'API token not valid',
+        ));
+    exit;
+  }
+}
+?>
--- index.php
+++ index.php
@@ -1,53 +1,18 @@
- 
 <?php
 include 'includes/settings.php';
 include 'includes/database.php';
 $db = new Database($db_host, $db_name, $db_user, $db_pass);
 
-/**
- * Handle API actions
- *
- * add a beverage: ?action=add&beverage=beer&amount=0.5&token=asdf_1234
- */
-if(isset($_GET['action']) && isset($_GET['token'])) {
-  $action = $_GET['action'];
-  $token = $_GET['token'];
-  if($db->isValidToken($token)){
-    $user = $db->getUserId($token);
-    switch($action){
-      case 'add':{
-        if(isset($_GET['beverage']) && isset($_GET['amount']) && is_numeric($_GET['amount'])){
-          $db->addBeverage($_GET['beverage'], $_GET['amount'], $user);
-          header('HTTP/1.1 200 OK');
-          echo '200';
-          exit;
-        }
-        else{
-          header('HTTP/1.1 400 Bad Request');
-          echo '400';
-          exit;
-        }
-      } break;
-    }
-  }
-  else{
-    header('HTTP/1.1 401 Unauthorized');
-    echo '401';
-    exit;
-  }
-}
-// Display the page.
-else{
-  // Add beverages to get the stats for here.
-  $beverages = array('beer');
+// Add beverages to get the stats for here.
+$beverages = array('beer');
 
-  $overall_stats = array('');
-  foreach ($beverages as $beverage) {
-    $overall_stats[$beverage] = array(
-      'count' => $db->getCount($beverage),
-      'amount' => $db->getAmount($beverage),
-      );
-  }
+$overall_stats = array('');
+foreach ($beverages as $beverage) {
+  $overall_stats[$beverage] = array(
+    'count' => $db->getCount($beverage),
+    'amount' => $db->getAmount($beverage),
+    );
+}
 ?>
 <html>
   <head>
@@ -58,7 +23,3 @@ else{
     <div class='count'>Beer amount: <?php echo $overall_stats['beer']['amount']; ?></div>
   </body>
 </html>
-<?php
-}
-
-?>
